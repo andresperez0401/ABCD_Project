3b7dde16aae071a30b08f0f04d2e0e279547762a
Andres Perez <andresperez0401@gmail.com>
2025-06-18 10:46:48 -0400

    Se agrego la bd sqlite

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,34 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, request
+from flask_migrate import Migrate, upgrade
 from flask_cors import CORS
+from dotenv import load_dotenv
+from api.models import db, Usuario  
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Permite solicitudes desde otros orígenes (React)
+
+
+#----------------------------------------------------- Base de Datos -----------------------------------------------------------------
+
+# 1) Armamos la ruta al archivo SQLite
+base_dir = os.path.dirname(os.path.realpath(__file__))  # carpeta backend/
+ruta_sqlite = os.path.join(base_dir, "sqlite", "abcdatabase.db")
+default_uri = f"sqlite:///{ruta_sqlite}"
+
+# 2) Usa DATABASE_URL si está definido, si no, SQLite local
+db_uri = os.getenv("DATABASE_URL", default_uri).replace("postgres://", "postgresql://")
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+Migrate(app, db, compare_type=True)
+
+#---------------------------------------------------- Termina la BD configuración -----------------------------------------------------
+
+#----------------------------------------------------- Rutas -----------------------------------------------------
 
 @app.route('/')
 def index():
